dbd18b726c78b42a7629f8a4257bc5f054743223
Lilian Dias <lilian.dias25@gmail.com>
2021-01-26 20:01:28 -0300

    add game end when the snake hits itself

--- script.js
+++ script.js
@@ -47,6 +47,13 @@ function initGame() {
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(game);
+      alert("GAME OVER :(");
+    }
+  }
+
   createdBackground();
   createdSnake();
   drawFood();
@@ -74,4 +81,4 @@ function initGame() {
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(initGame, 100);
+let game = setInterval(initGame, 100);
